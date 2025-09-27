6712c414eebbe8ba2685adad7221d7567cd2a315
Justin Rivera <jusrivera028@gmail.com>
2025-09-27 03:31:55 -0400

    [GZ] Add custom Apriltag models

--- tinkerbot_gazebo_sim/launch/launch_sim.launch.py
+++ tinkerbot_gazebo_sim/launch/launch_sim.launch.py
@@ -25,9 +25,8 @@ def generate_launch_description():
     world = LaunchConfiguration("world")
     world_arg = DeclareLaunchArgument(
         "world",
-        # default_value = 'empty.sdf',
         default_value=os.path.join(
-            get_package_share_directory(package_name), "worlds", "empty.world"
+            get_package_share_directory(package_name), "worlds", "apriltag.world"
         ),
         description="World to load",
     )
--- tinkerbot_gazebo_sim/setup.py
+++ tinkerbot_gazebo_sim/setup.py
@@ -1,31 +1,50 @@
-from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'tinkerbot_gazebo_sim'
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "tinkerbot_gazebo_sim"
+
+
+def get_model_data_files() -> list[tuple[str, list]]:
+    """Get a list of all targets & sources needed for building custom gazebo models"""
+    data_files = []
+    share_dir = os.path.join("share", PACKAGE_NAME)
+
+    # Walk models directory for all files
+    for root, _, files in os.walk("models"):
+        if not files:
+            continue
+
+        # Map source files to target share directory
+        sources = [os.path.join(root, file) for file in files]
+        target = os.path.join(share_dir, root)
+        data_files.append((target, sources))
+
+    return data_files
+
 
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    name=PACKAGE_NAME,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name), glob('description/*')),
-        (os.path.join('share', package_name), glob('config/*')),
-        (os.path.join('share', package_name, "worlds"), glob('worlds/*')),
-    ],
-    install_requires=['setuptools'],
+        ("share/ament_index/resource_index/packages", ["resource/" + PACKAGE_NAME]),
+        ("share/" + PACKAGE_NAME, ["package.xml"]),
+        (os.path.join("share", PACKAGE_NAME), glob("launch/*.launch.py")),
+        (os.path.join("share", PACKAGE_NAME), glob("description/*")),
+        (os.path.join("share", PACKAGE_NAME), glob("config/*")),
+        (os.path.join("share", PACKAGE_NAME, "worlds"), glob("worlds/*")),
+    ]
+    + get_model_data_files(),
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='',
-    maintainer_email='todo@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="",
+    maintainer_email="todo@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
